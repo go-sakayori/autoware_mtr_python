6deda444ccc17b913cb24e2e80dbde7464eae0b2
Daniel Sanchez <danielsanchezaran@gmail.com>
2024-12-13 17:57:43 +0900

    WIP: add code for ego localization and transform to normal traj
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

---
+++ autoware_mtr/dataclass/history.py
@@ -0,0 +1,140 @@
+from __future__ import annotations
+
+from collections import deque
+from dataclasses import dataclass
+from dataclasses import field
+from typing import Sequence
+
+import numpy as np
+
+from .agent import AgentState
+from .agent import AgentTrajectory
+from .agent import OriginalInfo
+
+__all__ = ("AgentHistory",)
+
+
+@dataclass
+class AgentHistory:
+    """A class to store agent history data."""
+
+    max_length: int
+    histories: dict[str, deque[AgentState]] = field(default_factory=dict, init=False)
+    infos: dict[str, OriginalInfo] = field(default_factory=dict, init=False)
+
+    def update(self, states: Sequence[AgentState], infos: Sequence[OriginalInfo]) -> None:
+        """Update history data.
+
+        Args:
+            states (Sequence[AgentState]): Sequence of AgentStates.
+        """
+        for state, info in zip(states, infos, strict=True):
+            self.update_state(state, info)
+
+    def update_state(self, state: AgentState, info: OriginalInfo | None = None) -> None:
+        """Update history state.
+
+        Args:
+            state (AgentState): Agent state.
+        """
+        uuid = state.uuid
+        if uuid not in self.histories:
+            self.histories[uuid] = deque(
+                [AgentState(uuid)] * self.max_length,
+                maxlen=self.max_length,
+            )
+        self.histories[uuid].append(state)
+        if info is not None:
+            self.infos[uuid] = info
+
+    def remove_invalid(self, current_timestamp: float, threshold: float) -> None:
+        """Remove agent histories whose the latest state are invalid or ancient.
+
+        Args:
+            current_timestamp (float): Current timestamp in [ms].
+            threshold (float): Threshold value to filter out ancient history in [ms].
+        """
+        new_histories = self.histories.copy()
+        new_infos = self.infos.copy()
+        for uuid, history in self.histories.items():
+            latest = history[-1]
+            if (not latest.is_valid) or self.is_ancient(
+                latest.timestamp, current_timestamp, threshold
+            ):
+                del new_histories[uuid]
+                if uuid in new_infos:
+                    del new_infos[uuid]
+
+        self.histories = new_histories
+        self.infos = new_infos
+
+    @staticmethod
+    def is_ancient(latest_timestamp: float, current_timestamp: float, threshold: float) -> bool:
+        """Check whether the latest state is ancient.
+
+        Args:
+            latest_timestamp (float): Latest state timestamp in [ms].
+            current_timestamp (float): Current timestamp in [ms].
+            threshold (float): Timestamp threshold in [ms].
+
+        Returns:
+            bool: Return True if timestamp difference is greater than threshold,
+                which means ancient.
+        """
+        timestamp_diff = abs(current_timestamp - latest_timestamp)
+        return timestamp_diff > threshold
+
+    def as_trajectory(self, *, latest: bool = False) -> tuple[AgentTrajectory, list[str]]:
+        """Convert agent history to AgentTrajectory.
+
+        Args:
+            latest (bool): Whether only to return the latest trajectory,
+                in the shape of (N, D). Defaults to False.
+
+        Returns:
+            tuple[AgentTrajectory, list[str]]: Instanced AgentTrajectory and the list of their uuids.
+        """
+        if latest:
+            return self._get_latest_trajectory()
+
+        num_agent = len(self.histories)
+        waypoints = np.zeros((num_agent, self.max_length, AgentTrajectory.num_dim))
+        label_ids = np.zeros(num_agent, dtype=np.int64)
+        uuids: list[str] = []
+        for n, (uuid, history) in enumerate(self.histories.items()):
+            uuids.append(uuid)
+            for t, state in enumerate(history):
+                waypoints[n, t] = (
+                    *state.xyz,
+                    *state.size,
+                    state.yaw,
+                    *state.vxy,
+                    state.is_valid,
+                )
+                label_ids[n] = state.label_id
+
+        return AgentTrajectory(waypoints, label_ids), uuids
+
+    def _get_latest_trajectory(self) -> tuple[AgentTrajectory, list[str]]:
+        """Return the latest agent state trajectory.
+
+        Returns:
+            tuple[AgentTrajectory, list[str]]: Instanced AgentTrajectory and the list of their uuids.
+        """
+        num_agent = len(self.histories)
+        waypoints = np.zeros((num_agent, AgentTrajectory.num_dim))
+        label_ids = np.zeros(num_agent, dtype=np.int64)
+        uuids: list[str] = []
+        for n, (uuid, history) in enumerate(self.histories.items()):
+            state = history[-1]
+            waypoints[n] = (
+                *state.xyz,
+                *state.size,
+                state.yaw,
+                *state.vxy,
+                state.is_valid,
+            )
+            label_ids[n] = state.label_id
+            uuids.append(uuid)
+
+        return AgentTrajectory(waypoints, label_ids), uuids
--- src/mtr_node.py
+++ src/mtr_node.py
@@ -14,6 +14,7 @@ from rclpy.qos import QoSReliabilityPolicy
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 import yaml
+import torch
 
 
 from awml_pred.common import Config, create_logger, get_num_devices, init_dist_pytorch, init_dist_slurm, load_checkpoint
@@ -22,7 +23,7 @@ from awml_pred.deploy.apis.torch2onnx import _load_inputs
 from autoware_mtr.conversion.ego import from_odometry
 from autoware_mtr.conversion.misc import timestamp2ms
 from autoware_mtr.datatype import AgentLabel
-
+from autoware_mtr.dataclass.history import AgentHistory
 
 class MTRNode(Node):
     def __init__(self) -> None:
@@ -68,7 +69,19 @@ class MTRNode(Node):
             .string_value
         )
 
-        # self._history = AgentHistory(max_length=num_timestamp)
+        num_timestamp = (
+            self.declare_parameter("num_timestamp", descriptor=descriptor)
+            .get_parameter_value()
+            .integer_value
+        )
+
+        self._timestamp_threshold = (
+            self.declare_parameter("timestamp_threshold", descriptor=descriptor)
+            .get_parameter_value()
+            .double_value
+        )
+
+        self._history = AgentHistory(max_length=num_timestamp)
 
 
         cfg = Config.from_file(model_config_path)
@@ -83,22 +96,18 @@ class MTRNode(Node):
         self.model.eval()
         self.model.cuda()
         self.model, _ = load_checkpoint(self.model, checkpoint_path, is_distributed=is_distributed)
-        print(self.model.named_buffers)
-        deploy_cfg = Config.from_file(deploy_config_path)
-        dummy_input = _load_inputs(deploy_cfg.input_shapes)
-
-        print(self.model(**dummy_input))
+        self.deploy_cfg = Config.from_file(deploy_config_path)
 
         if build_only:
             exit(0)
 
-        # self._tf_buffer = Buffer()
-        # self._tf_listener = TransformListener(self._tf_buffer, self)
+        self._tf_buffer = Buffer()
+        self._tf_listener = TransformListener(self._tf_buffer, self)
 
     def _callback(self, msg: Odometry) -> None:
         # remove invalid ancient agent history
         timestamp = timestamp2ms(msg.header)
-        # self._history.remove_invalid(timestamp, self._timestamp_threshold)
+        self._history.remove_invalid(timestamp, self._timestamp_threshold)
 
         # update agent history
         current_ego, info = from_odometry(
@@ -107,22 +116,24 @@ class MTRNode(Node):
             label_id=AgentLabel.VEHICLE,
             size=(4.0, 2.0, 1.0),  # size is unused dummy
         )
-        print("current_ego.xyz",current_ego.xyz)
-        # self._history.update_state(current_ego, info)
+        # print("current_ego.xyz",current_ego.xyz)
+        self._history.update_state(current_ego, info)
+
+        dummy_input = _load_inputs(self.deploy_cfg.input_shapes)
+        # print(self.model(**dummy_input))
+
 
         # pre-process
         # inputs = self._preprocess(self._history, current_ego, self._lane_segments)
 
         # # inference
 
-        # inputs = inputs.cuda()
-        # with torch.no_grad():
-        #     pred_scores, pred_trajs = self._model(
-        #         inputs.actor,
-        #         inputs.lane,
-        #         inputs.rpe,
-        #         inputs.rpe_mask,
-        #     )
+        with torch.no_grad():
+            pred_scores, pred_trajs = self.model(**dummy_input)
+
+        print("pred_scores.shape() ",pred_scores.shape)
+        print("pred_trajs.shape()", pred_trajs.shape)
+
         # # post-process
         # pred_scores, pred_trajs = self._postprocess(pred_scores, pred_trajs)
 
@@ -135,6 +146,50 @@ class MTRNode(Node):
         #     score_threshold=self._score_threshold,
         # )
         # self._publisher.publish(pred_objs)
+
+   def _postprocess(
+        self,
+        pred_scores: NDArray | torch.Tensor,
+        pred_trajs: NDArray | torch.Tensor,
+    ) -> tuple[NDArray, NDArray]:
+        """Run postprocess.
+
+        Args:
+            pred_scores (NDArray | torch.Tensor): Predicted scores in the shape of
+                (N, M).
+            pred_trajs (NDArray | torch.Tensor): Predicted trajectories in the shape of
+                (N, M, T, 4).
+
+        Returns:
+            tuple[NDArray, NDArray]: Transformed and sorted prediction.
+        """
+        if isinstance(pred_scores, torch.Tensor):
+            pred_scores = pred_scores.cpu().detach().numpy()
+        if isinstance(pred_trajs, torch.Tensor):
+            pred_trajs = pred_trajs.cpu().detach().numpy()
+
+        # predicted traj point info is X,Y,Xmean,Ymean,Variance,Vx,Vy
+
+        num_agent, num_mode, num_future, num_feat = pred_trajs.shape
+        assert num_feat == 7, f"Expected predicted feature is (x, y, vx, vy), but got {num_feat}"
+
+        # transform from agent centric coords to world coords
+        current_agent, _ = self._history.as_trajectory(latest=True)
+
+        #first 2 elements are xy then we use the negative ego rotation. reshape, I dont know
+        # each trajectory is in  the ref frame of its own agent. For ego we might use TF ?
+        pred_trajs[..., :2] = rotate_along_z(
+            pred_trajs.reshape(num_agent, -1, num_feat)[..., :2], -current_agent.yaw
+        ).reshape(num_agent, num_mode, num_future, 2)
+        pred_trajs[..., :2] += current_agent.xy[:, None, None, :]
+
+        # sort by score
+        pred_scores = softmax(pred_scores, axis=1)
+        sort_indices = np.argsort(-pred_scores, axis=1)
+        pred_scores = np.take_along_axis(pred_scores, sort_indices, axis=1)
+        pred_trajs = np.take_along_axis(pred_trajs, sort_indices[..., None, None], axis=1)
+
+        return pred_scores, pred_trajs
 
 
 
